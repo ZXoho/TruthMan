d9fb42c95efcead48a3cd333a818b04375bfb382
ZXoho <348105852@qq.com>
2018-05-18 15:26:44 +0800

    Initial commit

---
+++ src/main/java/com/cn/thruth/ThruthApplication.java
@@ -0,0 +1,12 @@
+package com.cn.thruth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThruthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThruthApplication.class, args);
+	}
+}
